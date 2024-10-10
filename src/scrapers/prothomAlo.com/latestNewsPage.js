866e8c34581cc52606427fded60d72ac3493eb26
pranta <you@example.com>
2024-10-10 16:50:31 +0600

    working on scraping on prothomalo

--- src/scrapers/prothomAlo.com/latestNewsPage.js
+++ src/scrapers/prothomAlo.com/latestNewsPage.js
@@ -1,4 +1,5 @@
 const gotoPage = require("../shared/utilities/gotoPage")
+const remoteContentText = ["আরও পড়ুন", "বিজ্ঞাপন"]
 
 const scrapeProthomAlo = async () => {
   const { page, browser } = await gotoPage('https://www.prothomalo.com/collection/latest')
@@ -17,43 +18,33 @@ const scrapeProthomAlo = async () => {
 
     return articles
   });
-
-  await pageEvaluate.forEach(async (pageInfo) => {
+  console.log("pageEvaluate =>", pageEvaluate)
+  let contentList = await (await Promise.all(pageEvaluate.map(async (pageInfo) => {
     const { page, browser } = await gotoPage(pageInfo.pageUrl)
     const newsDetailsPageEvaluate = await page.evaluate(async () => {
-      let articles = [];
-      const title = document.querySelector('.IiRps')?.innerText;
-      const newsContainer = document.querySelector('.story-content');
-      // const images = newsContainer.querySelectorAll('img')?.forEach((img) => { return img.src })
-      const pictures = newsContainer.querySelectorAll(".qt-image")
-      const footer = newsContainer.querySelectorAll(".print-footer-row2")
-      await pictures.forEach((element) => {
-        const img = element.querySelector("img")
-        if (img?.src) {
-          articles.push(`${img.src}`)
+      let articles = {}
+      const title = await document.querySelector('.IiRps')?.innerText;
+      const newsContainer = await document.querySelector('.story-content');
+      const images = await newsContainer ? Array.from(newsContainer.querySelectorAll('img')).map(img => img?.src) : [];
+      const contentBody = newsContainer.querySelector('.VzzDZ')
 
-        }
-      })
-
-      articles.push({ title, length: footer.length })
-      // contentContainer.forEach(article => {
-      //   const pageUrl = article.querySelector('a')?.href
-
-      //   if (pageUrl) {
-      //     articles.push({
-      //       pageUrl,
-      //     });
-      //   }
-      // });
+    const filteredElement =  await [...contentBody.children].filter(async (element) => {
+        const stringEle = element.innerHTML;
+        console.log("Inner content as string:", stringEle);
+        const existRemoveElement = remoteContentText.some((text) => stringEle.includes(text))
+        return existRemoveElement
+      });
+      if (title && images?.length && contentBody) {
+        articles = { title, images, contentBody: `${contentBody}`, contentLength: contentBody.length || 0, filteredElement: [...filteredElement] }
+      }
 
       return articles
-    });
+    })
 
-    console.log("innerDetails ==>>", newsDetailsPageEvaluate)
     await browser.close();
-
-  })
-
+    return newsDetailsPageEvaluate
+  }))).filter((content) => Object.keys(content).length > 0)
+  console.log("contentList ==>>", contentList)
   await browser.close();
 }
 
@@ -61,4 +52,7 @@ const scrapeProthomAlo = async () => {
 
 module.exports = scrapeProthomAlo();
 
+const sub = ["1", "2"]
+const main = "12 sd3 5 s"
 
+main.includes(sub[0] || sub[1])
